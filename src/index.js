ff6210dde86f5e64d24b5e7742ad5a404c6016ff
VioletQQY <forviolet771@gmail.com>
2019-01-17 10:05:49 +0800

    init simple server side render home page

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// const express = require('express');
+// const React = require('react');
+// const renderToString = require('react-dom/server').renderToString;
+// const Home = require('./client/components/Home').default;
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   const content = renderToString(<Home />);
+
+//   res.send(content);
+// });
+
+// app.listen(3000, () => {
+//   console.log('Listening on port 3000');
+// });
+
+import express from 'express';
+// import React from 'react';
+// import { renderToString } from 'react-dom//server';
+// import Home from './client/components/Home';
+import renderer from './helpers/renderer';
+
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  // const content = renderToString(<Home />);
+
+  // const html = `
+  //   <html>
+  //     <head></head>
+  //     <body>
+  //       <div id="root">${content}</div>
+  //       <script src="bundle.js"></script>
+  //     </body>
+  //   </html>
+  // `;
+
+  res.send(renderer());
+});
+
+app.listen(3003, () => {
+  console.log('Listening on port 3003');
+});
